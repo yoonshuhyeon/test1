feedb1c9904cb5d4abab278f769d123028ab638a
yoonshuhyeon <138233074+yoonshuhyeon@users.noreply.github.com>
2025-07-21 01:10:37 +0900

    Update sever.py

--- sever.py
+++ sever.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 CORS(app)
 
 # portal-project 폴더의 절대 경로 설정
-PORTAL_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'portal-project'))
+PORTAL_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # =================================
 # CONFIGURATION
@@ -330,4 +330,6 @@ with app.app_context():
         # Column already exists, which is fine.
         print(f"Could not add column: {e}")
 
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)

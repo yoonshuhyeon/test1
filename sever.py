f374d7576330cf90f3235743822ac9a208e4ca8a
yoonshuhyeon <138233074+yoonshuhyeon@users.noreply.github.com>
2025-07-29 20:03:52 +0900

    Update sever.py

--- sever.py
+++ sever.py
@@ -28,7 +28,7 @@ if not uri:
     # 로컬 테스트용 SQLite 데이터베이스 설정
     uri = f"sqlite:///{os.path.join(os.path.dirname(os.path.abspath(__file__)), 'local_database.db')}"
 elif uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
+    uri = uri.replace("postgres://", "postgresql+pg8000://", 1)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
